e3ac91774938389763c9b1376041acb74ddd2f01
Julio Esteban Cáceres Sanzana <Julio@Macbook-M1-Pro.local>
2023-08-18 00:29:47 -0400

    Mejoras varias

--- server/server.js
+++ server/server.js
@@ -1,44 +1,47 @@
-const express = require('express');
-const puppeteer = require('puppeteer');
-const bodyParser = require('body-parser');
-const Handlebars = require('handlebars');
-const fs = require('fs');
-const cors = require('cors');
+const express = require("express");
+const puppeteer = require("puppeteer");
+const bodyParser = require("body-parser");
+const Handlebars = require("handlebars");
+const fs = require("fs");
+const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "50mb" }));
 
 // Lee el contenido del archivo template.hbs
-const templateHtml = fs.readFileSync('./resources/templates/template.hbs', 'utf-8');
+const templateHtml = fs.readFileSync(
+	"./resources/templates/template.hbs",
+	"utf-8",
+);
 
-app.post('/generate-image', async (req, res) => {
-    const { image, texto1, texto2 } = req.body;
+app.post("/generate-image", async (req, res) => {
+	const { image, texto1, texto2 } = req.body;
+	let browser;
+	try {
+		browser = await puppeteer.launch({ headless: "new" });
 
-    try {
-        const browser = await puppeteer.launch({ headless: true });
+		const page = await browser.newPage();
 
-        const page = await browser.newPage();
-
-        // Compila el template Handlebars
-        const template = Handlebars.compile(templateHtml);
-        const finalHtml = template({ image, texto1, texto2 });
-        await page.setContent(finalHtml);
-
-        const screenshot = await page.screenshot();
-
-        await browser.close();
-
-        res.send(screenshot);
-
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).send("Error generando la imagen.");
-    }
+		// Compila el template Handlebars
+		const template = Handlebars.compile(templateHtml);
+		const finalHtml = template({ image, texto1, texto2 });
+		await page.setContent(finalHtml);
+		await page.waitForSelector("img");
+		const screenshot = await page.screenshot();
+		res.send(screenshot);
+	} catch (error) {
+		console.error("Error:", error);
+		res.status(500).send("Error generando la imagen.");
+	} finally {
+		if (browser) {
+			await browser.close();
+		}
+	}
 });
 
 const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
